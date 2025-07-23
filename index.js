4b51ae4b6b740ad9bd2205c19b8f61a7f4b6fe9d
RM <amohammad22589@gmail.com>
2025-07-23 14:58:53 -0400

    Done with main, but still need to work on styling and buttons

--- index.js
+++ index.js
@@ -2,22 +2,36 @@
 //Getting the radio buttons + value
 
 //get this function checked later
-document.addEventListener('DOMContentLoaded', function() {
+const checkRadioButton = () => {
     const radioButtons = document.getElementsByName('user-picked');
+    for (let radioButton of radioButtons) {
+        if (radioButton.checked) {
+            return radioButton.value;
+        }
+    };
+    throw new Error('No Radio Buttons Checked');
+}
 
-    radioButtons.forEach(radioButton => {
-        let userChose;
-        radioButton.addEventListener('change', function() {
-            if (this.checked) { // Check if the current radio button is the one selected
-              userChose = this.value;
-            }
-        });
-    });
-
-    return userChose;
-});
 
 //updating the image
+
+const userRock = document.getElementById('user-rock');
+const userPaper = document.getElementById('user-paper');
+const userScissors = document.getElementById('user-scissors');
+
+const getUserImage = (userChose) => {
+    if (userChose === 'rock'){
+        userRock.style.display = 'block';
+    } 
+    if (userChose === 'paper') {
+        userPaper.style.display = 'block';
+    }
+    if (userChose === 'scissors') {
+        userScissors.style.display = 'block';
+    }
+}
+
+
 
 //getting computer choice
 //Function that generates random number (0, 1, 2)
@@ -27,7 +41,6 @@ const getRandom = () => {
     return randomNum;
 }
 
-let random = getRandom();
 
 //function that determines the computer choice
     const getComputerChoice = (randomNum) => {
@@ -40,7 +53,6 @@ let random = getRandom();
         }
     }
 
-let computerChoice = getComputerChoice(random);
 
 //the test
 //console.log(`random is ${random}| computer choice is ${computerChoice}`);
@@ -63,4 +75,77 @@ const getComputerChoiceImage = (computerChoice) => {
     }
 }
 
+
 //Determining winner
+
+const determineWinner = (userValue, computerValue) => {
+    if (userValue === computerValue) {
+        return 'tie';
+    } else if (userValue === 'rock' && computerValue === 'scissors') {
+        return 'userWins';
+    } else if (userValue === 'paper' && computerValue === 'scissors') {
+        return 'computerWins';
+    } else if (userValue === 'scissors' && computerValue === 'rock') {
+        return 'userWins';
+    } else if (userValue == 'rock' && computerValue == 'paper') {
+        return 'computerWins';
+    } else if (userValue === 'paper' && computerValue === 'rock') {
+        return 'userWins';
+    } else if (userValue === 'scissors' && computerValue === 'rock') {
+        return 'computerWins';
+    }
+}
+
+
+//Displaying the correct image
+
+const userWins = document.getElementById('user-won');
+const computerWins = document.getElementById('computer-won');
+const tie = document.getElementById('tie');
+
+const displayingImage = (winnerIs) => {
+    if (winnerIs == 'userWins'){
+        userWins.style.display = 'block';
+    }
+
+    if (winnerIs == 'computerWins'){
+        computerWins.style.display = 'block';
+    }
+
+    if (winnerIs == 'tie'){
+        tie.style.display = 'block';
+    }
+
+    setTimeout(function() {
+        userWins.style.display = 'none';
+        computerWins.style.display = 'none';
+        tie.style.display = 'none';
+  }, 5000);
+}
+
+
+
+//run game
+const runGame = () => {
+
+    try {
+        const userValue = checkRadioButton();
+        let random = getRandom();
+        let computerValue = getComputerChoice(random);
+
+        getUserImage(userValue);
+
+        setTimeout(function() {
+            getComputerChoiceImage(computerValue);
+    }, 3000);
+
+        setTimeout(function() {
+            const winnerIs = determineWinner(userValue, computerValue);
+            displayingImage(winnerIs);
+    }, 5000);
+        } catch (error) {
+            console.log(error.message)
+        }
+
+
+}
